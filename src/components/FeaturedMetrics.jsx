d19b4509401669692f9a76d31febe7fdfacd466c
NICOLAS CHABANOVSKY <nicolas.chabanovsky@gmail.com>
2025-03-10 10:24:40 +0100

    added new metrics and their values

--- src/components/FeaturedMetrics.jsx
+++ src/components/FeaturedMetrics.jsx
@@ -19,13 +19,7 @@ var stickiness = new Metric(
   "", // DAU/MAU * 100.
   "How many monthly active users that are active on a given day. We calculate it daily and then average over the month. Monthly active users is calculated as a sliding 30-day window."
 );
-var utilisation = new Metric(
-  45.56, 47.80, 37.84,
-  "%",
-  "Utilization",
-  "", // DAU / Users alive * 100
-  "How many users are active among those who could act."
-);
+
 var time_to_answer = new Metric(
   31.46, 29.70, 37.49,
   " hrs",
@@ -33,22 +27,96 @@ var time_to_answer = new Metric(
   "",
   "Time between starting a discussion and the first reply appearing in it."
 );
-var churn_rate = new Metric(
-  20.65, 20.62, 20.77,
+
+var dau = new Metric(
+  18, 23, 12,
   "%",
-  "Churn rate",
-  "", // Users left / Users alive * 100
-  "How many users left the community among those whom we consider alive."
-);
-var community_growth = new Metric(
-  2.44, 3.84, -2.43,
-  "%",
-  "Community growth rate",
+  "Daily active users",
   "",
-  "How fast the number of alive users is growing."
+  "The number of users who have been active in the community during a day."
 );
 
-var metrics = [stickiness, utilisation, time_to_answer, churn_rate, community_growth];
+var daily_utilisation = new Metric(
+  4.56, 5.78, 2.89,
+  "%",
+  "Daily user base utilisation",
+  "",
+  "The percent of active users among those who are alive, calculated daily."
+);
+
+var daily_churn = new Metric(
+  0.79, 0.91, 0.57,
+  "%",
+  "Daily user churn",
+  "",
+  "The rate at which users stop participating in a community, calculated daily."
+);
+
+var daily_growth = new Metric(
+  0.1, 0.23, 0.02,
+  "%",
+  "Daily user growth",
+  "",
+  "The rate at which users start participating in the community."
+);
+
+var mau = new Metric(
+  209, 270, 136,
+  "users",
+  "Monthly active users",
+  "",
+  "The number of users who have posted in the community."
+);
+
+var montly_utilisation = new Metric(
+  36.91, 43.98, 28.21,
+  "%",
+  "Monthly user base utilisation",
+  "", 
+  "The percent of active users among those who are alive, calculated monthly"
+);
+
+var monthly_churn = new Metric(
+  15.76, 17.6, 13.51,
+  "%",
+  "Monthly user churn",
+  "", // Users left / Users alive * 100
+  "The rate at which users stop participating in a community, calculated monthly"
+);
+
+var montly_growth = new Metric(
+  15.14, 27.89, -0.35,
+  "%",
+  "Monthly user growth",
+  "",
+  "The rate at which users start participating in the community."
+);
+
+var new_users_monthly = new Metric(
+  94, 125, 56,
+  "users",
+  "New active users monthly",
+  "",
+  "The number of users who performed their first action on the platform during a given month."
+);
+
+var user_tenure = new Metric(
+  54.27, 65.52, 40.97,
+  "months",
+  "User tenure by month",
+  "",
+  "Time between the first and the last actions of a user in the community."
+);
+
+var num_of_actions = new Metric(
+  11.72, 14.76, 8.03,
+  "actions",
+  "Number of actions by month",
+  "",
+  "Total number of actions performed by a user during their tenure."
+);
+
+var metrics = [stickiness, utilisation, time_to_answer, dau, daily_utilisation, daily_churn, daily_growth, mau, montly_utilisation, monthly_churn, montly_growth, new_users_monthly, user_tenure, num_of_actions];
 
 function FeaturedMetrics() {
   return (
